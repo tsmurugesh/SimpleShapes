c070c06f508e6e9888711e3d456e4652b2a22476
tmurugesh <73613956+tmurugesh@users.noreply.github.com>
2021-02-02 15:33:34 -0800

    added shape 3

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function draw() {
 }
 
 function myEye(){
-	// 2 shapes; circle and a square
+	// shapes 1 & 2; circle and a square
   noStroke();	
   fill(255);
   ellipse (mouseX, mouseY, 100,100);
@@ -27,8 +27,21 @@ function myEye(){
 }
 
 function myTriangle(){
-  stroke(3);
-  noFill();
+	// shape 3 made color changing triangle
+  if (mouseX > 600 && mouseY < 400){
+  	fill(2,20,20);
+  }
+  else if (mouseX > 600 && mouseY > 400){
+  	fill(40,250,20);
+  }
+  else if (mouseX < 600 && mouseY > 400){
+  	fill(200,250,60);
+  }
+  else{
+  	fill(10,30,200);
+  }
   triangle(500, 230, 370, 400, 550, 600);
-  //if (mouseX >= )
+
+
+
 }
