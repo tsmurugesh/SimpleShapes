4a4bb872609899b5990e6ef864faa839d93d4627
tmurugesh <73613956+tmurugesh@users.noreply.github.com>
2021-02-02 15:22:57 -0800

    updated code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 // Setup code goes here
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(1200, 800);
   textSize(40);
   textAlign(CENTER);
  }
@@ -9,6 +9,26 @@ function setup() {
 
 // Draw code goes here
 function draw() {
-// pink bg
+  // light pink bg
   background('#ffb2e0');
+  myEye();
+  myTriangle();
+
+}
+
+function myEye(){
+	// 2 shapes; circle and a square
+  noStroke();	
+  fill(255);
+  ellipse (mouseX, mouseY, 100,100);
+  fill('blue');
+  rect (pmouseX+1, pmouseY+1, 20,20);
+
+}
+
+function myTriangle(){
+  stroke(3);
+  noFill();
+  triangle(500, 230, 370, 400, 550, 600);
+  //if (mouseX >= )
 }
